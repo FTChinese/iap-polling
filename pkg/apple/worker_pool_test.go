7bc5c5e1e37aa4615fe4f7dfc30760fd536ea370
weiguo.ni <neefrankie@gmail.com>
2020-09-24 17:48:36 +0800

    test

--- pkg/apple/worker_pool_test.go
+++ pkg/apple/worker_pool_test.go
@@ -1,7 +1,10 @@
 package apple
 
 import (
+	"context"
 	"github.com/FTChinese.com/iap-polling/pkg/config"
+	"github.com/FTChinese.com/iap-polling/pkg/db"
+	"github.com/go-redis/redis/v8"
 	"go.uber.org/zap/zaptest"
 	"testing"
 )
@@ -9,7 +12,20 @@ import (
 func TestVerifier_Start(t *testing.T) {
 	config.MustSetupViper()
 
-	v := NewVerifier(false, zaptest.NewLogger(t))
+	// Uses IAP production.
+	v := &Verifier{
+		db:         db.MustNewDB(config.MustDBConn(false)),
+		vrfClient:  NewVerificationClient(true),
+		subsClient: NewSubsClient(false),
+		rdb: redis.NewClient(&redis.Options{
+			Addr:     config.MustRedisAddress().Pick(false),
+			Password: "",
+			DB:       0,
+		}),
+		writer: NewKafkaWriter(config.MustKafkaAddress().PickSlice(false)),
+		logger: zaptest.NewLogger(t),
+		ctx:    context.Background(),
+	}
 
 	err := v.Start()
 	if err != nil {
