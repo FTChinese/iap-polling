dcd2214504bf3da07e96f18b3f97c3d7610372ef
weiguo.ni <neefrankie@gmail.com>
2020-11-30 17:35:38 +0800

    refactor: removed cli flags

--- cmd/migrate-link/main.go
+++ cmd/migrate-link/main.go
@@ -1,34 +1,21 @@
 package main
 
 import (
-	"flag"
 	"github.com/FTChinese.com/iap-polling/pkg/apple"
 	"github.com/FTChinese.com/iap-polling/pkg/config"
 	"go.uber.org/zap"
 	"log"
 )
 
-var (
-	production bool // Determine whether hit production api.
-	dir        string
-)
-
-func init() {
-	flag.BoolVar(&production, "production", false, "Send verification request to production api or localhost")
-	flag.StringVar(&dir, "dir", "", "Which directory to read receipts")
-
-	flag.Parse()
-
-	config.MustSetupViper()
-}
-
 func main() {
+	config.MustSetupViper()
+
 	logger, err := zap.NewProduction()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	migrator := apple.NewLinkMigration(production, logger)
+	migrator := apple.NewLinkMigration(logger)
 
 	err = migrator.Start()
 	if err != nil {
